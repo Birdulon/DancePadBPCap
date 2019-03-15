b27f8c7779773f9c621871c2966abd8351ef3afd
Luke Hubmayer-Werner <mongoose41@gmail.com>
2019-03-15 21:23:49 +1030

    Fatmat changes

--- DancePadBPCap.ino
+++ DancePadBPCap.ino
@@ -3,19 +3,23 @@ USBCompositeSerial CompositeSerial;
 USBHID HID;
 HIDJoystick Joystick(HID);
 
+#define MANUFACTURER "LHW"
+#define PRODUCT "FatMat" //"FlatMat"
+
 //const uint8_t reportDescription[] = {
 //   //HID_MOUSE_REPORT_DESCRIPTOR(),
 //   //HID_KEYBOARD_REPORT_DESCRIPTOR(),
 //   HID_JOYSTICK_REPORT_DESCRIPTOR(),
 //};
 
-#define MODE INPUT_PULLDOWN
+#define JOYSTICK_AXES
+
 const uint8_t btn_pins[] = {PA0, PA1, PA2, PA3, PA4, PA5};
 const uint8_t adc_gnd_pin = PB1;
 const int NUM_BTNS = sizeof(btn_pins)/sizeof(btn_pins[0]);
 const int us_per_loop = 2000;  // We want 500Hz polling
-const int NUM_SAMPLES = 96;
-const int NUM_SAMPLES_ELIMINATE = 32/2;
+const int NUM_SAMPLES = 100;
+const int NUM_SAMPLES_ELIMINATE = 36/2;
 const int NUM_SAMPLES_SHIFT = 6;
 
 uint32_t btn_values[NUM_BTNS] = {0};
@@ -32,10 +36,12 @@ void setup() {
     pinMode(btn_pins[i], OUTPUT);
     digitalWrite(btn_pins[i], LOW);
   }
+  //ADC_SMPR_7_5 ADC_SMPR_28_5 ADC_SMPR_41_5
+  ADC1_BASE->SMPR2 = ADC_SMPR_7_5 <<(9*3);
   calibrate();
   
-  USBComposite.setManufacturerString("Luke's");
-  USBComposite.setProductString("FlatMat");
+  USBComposite.setManufacturerString(MANUFACTURER);
+  USBComposite.setProductString(PRODUCT);
   Joystick.setManualReportMode(true);
   
   HID.begin(HID_JOYSTICK);
@@ -45,9 +51,7 @@ void setup() {
 void scan_pads(uint32_t output[NUM_BTNS]) {
   for (int i=0; i<NUM_BTNS; i++){
     pinMode(btn_pins[i], INPUT_PULLUP);
-    //unsigned long start_pin_us = micros();  // may have to re-add for timing purposes
     analogRead(adc_gnd_pin);
-    analogRead(adc_gnd_pin);  // discharge for twice as long for good measure
     output[i] = analogRead(btn_pins[i]);
     pinMode(btn_pins[i], OUTPUT);
     digitalWrite(btn_pins[i], LOW);
@@ -101,6 +105,9 @@ void calibrate() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
+
+static const float thresh_off = 0.95; //0.99; //0.9995; //0.999999;
+static const float thresh_on = 0.9; //0.99; //0.996;
 void loop() {
   unsigned long start_us = micros();
   if (!digitalRead(PB0))
@@ -111,17 +118,19 @@ void loop() {
     for (int j=1; j<NUM_SAMPLES; j++)
       btn_values[i] += btn_values_raw[j][i];
     btn_values[i] >>= NUM_SAMPLES_SHIFT;
-    float multiplier = btn_last_states[i] ? 0.999999 : 0.996;
+    float multiplier = btn_last_states[i] ? thresh_off : thresh_on;
     bool new_state = btn_values[i] < btn_values_min[i]*multiplier;
     Joystick.button(i+1, new_state);
     btn_last_states[i] = new_state;
   }
-//  Joystick.X(btn_values[0]>>2);
-//  Joystick.Y(btn_values[1]>>2);
-//  Joystick.Xrotate(btn_values[2]>>2);
-//  Joystick.Yrotate(btn_values[3]>>2);
-//  Joystick.sliderLeft(btn_values[4]>>2);
-//  Joystick.sliderRight(btn_values[5]>>2);
+  #ifdef JOYSTICK_AXES
+    Joystick.X(btn_values[0]>>2);
+    Joystick.Y(btn_values[1]>>2);
+    Joystick.Xrotate(btn_values[2]>>2);
+    Joystick.Yrotate(btn_values[3]>>2);
+    Joystick.sliderLeft(btn_values[4]>>2);
+    Joystick.sliderRight(btn_values[5]>>2);
+  #endif
   Joystick.send();
 //  char str[130];
 //  sprintf(str, "Buttons: %d  %d  %d  %d  %d  %d", btn_values[0], btn_values[1], btn_values[2], btn_values[3], btn_values[4], btn_values[5]);
